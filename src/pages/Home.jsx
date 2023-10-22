eca3ecbefd3d0dd2e38ec5bbb28dd06ba8f6f362
Madina <junmadina@mail.ru>
2023-10-22 20:03:29 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({
+    name: "популярности",
+    sortProps: "rating",
+  });
+  useEffect(() => {
+    setIsLoading(true);
+    const order = sortType.sortProps.includes("-") ? "asc" : "desc";
+    const sortBy = sortType.sortProps.replace("-", "");
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://6373ef27716c2e19165485e9.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
+
+  console.log("sort", sortType);
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          value={categoryId}
+          OnClickCategory={(i) => setCategoryId(i)}
+        />
+        <Sort value={sortType} onClickSortType={(i) => setSortType(i)} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => (
+              <PizzaBlock
+                key={obj.id}
+                title={obj.title}
+                price={obj.price}
+                image={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
